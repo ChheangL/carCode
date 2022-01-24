2201eeb23ecd9d589ac5727db9aefa4d201b3984
chheang ly <lychheang008@gmail.com>
2022-01-24 15:47:43 +0700

    update

--- Testing.py
+++ Testing.py
@@ -1,15 +1,17 @@
 from auto_drive_functions import retrieve_angle
 from ImageFrame import Frame
+from PIL import Image
 import cv2
 
-frame1 = Frame(1280,720,10)
-cam = cv2.VideoCapture(0)
+frame1 = Frame(1000,667,10)
+img = Image.open('road1.jpg')
+#cam = cv2.VideoCapture(0)
 
 
 def main():
     while True:
-        img = cv2.cvtColor(cam.read()[1],cv2.COLOR_BGR2GRAY)
-        angle = retrieve_angle(s1= 60,hd=5,img = img,frame = frame1,plot=False)
+        #img = cv2.cvtColor(cam.read()[1],cv2.COLOR_BGR2GRAY)
+        angle = retrieve_angle(s1= 60,hd=5,img_path = img,frame = frame1)
         print(angle)
     
 main()
