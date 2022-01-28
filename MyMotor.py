b9a63234f8ff5d21e6dbaeaaad0083c23e5a43fc
chheang ly <lychheang008@gmail.com>
2022-01-28 12:13:49 +0700

    reduce Copload

---
+++ MyMotor.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+
+class Mymotor :
+    
+    def __init__(self,in1,in2,en):
+        GPIO.cleanup()
+        self.in1 = in1
+        GPIO.setup(self.in1,GPIO.OUT)
+        self.in2 = in2
+        GPIO.setup(self.in2,GPIO.OUT)
+        self.en = en
+        GPIO.setup(self.en,GPIO.OUT)
+        GPIO.output(self.in1,GPIO.LOW)
+        GPIO.output(self.in2,GPIO.LOW)
+        self.p=GPIO.PWM(self.en,1000)
+        print('init-motor complete')
+
+
+    
+    def move_forward(self,speed):
+        self.p.start((lambda x: speed if speed>=0 and speed <=100 else 0))
+        GPIO.output(self.in1,GPIO.HIGH)
+        GPIO.output(self.in2,GPIO.LOW)
+        print('moving forward')
+        
+    def move_backward(self,speed):
+        self.p.start((lambda x: speed if speed>=0 and speed <=100 else 0))
+        GPIO.output(self.in1,GPIO.LOW)
+        GPIO.output(self.in2,GPIO.HIGH)
+        print('moving backward')
+        
+    def stop(self):
+        GPIO.output(self.in1,GPIO.LOW)
+        GPIO.output(self.in2,GPIO.LOW)
+        print('stopped')
