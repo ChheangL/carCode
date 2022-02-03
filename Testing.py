107ed51a23f559a7fcc38d83550f4e24566be16c
chheangLy <lychheang008@gmail.com>
2022-02-03 09:27:26 +0000

    update

--- Testing.py
+++ Testing.py
@@ -5,6 +5,7 @@ from time import sleep
 import cv2
 import numpy as np
 import math
+import timeit
 frame1 = Frame(640,480,10)
 
 cam = cv2.VideoCapture(0)
@@ -25,33 +26,40 @@ def servo_begin(servo, angle):
 		print("Angle: ", angle)
 		angleValue = (2/180 * angle - 1)
 		servo.value = angleValue
-		sleep(1)
+		#sleep(1)
 	except KeyboardInterrupt:
 		print("Program stopped")
 
 
 def main():
-	print('move forward')
-	motor.forward(1)
+	#print('move forward')
+	#motor.forward(1)
+	x = 1
 	while True:
-		img = np.array(cam.read()[1])
+		start = timeit.default_timer()
+		img = cam.read()[1]
+		
+		#cv2.imwrite('~/Desktop/testimage/img'+str(x)+'.jpg', img)
+		x=x+1
+		
 		#print(img.shape)
 		myAngle = retrieve_angle(s1=60, hd=5, img_path=img, frame=frame1)
 		print(myAngle.shape)
-		if math.isnan(myAngle[1]) : continue
+		
 		if myAngle.shape > (1,):
-			myAngle = int((myAngle[1] + myAngle[0])/2)
+			if math.isnan(myAngle[1]) : continue
+			myAngle = myAngle[1]
 		else:
 			myAngle = 90
-
+		
 		if myAngle <= 80 or myAngle >= 100 :
 			print('slow')
 			motor.forward(0.25)
 		else:
 			print('fast')
-			motor.forward(0.5)
-
+			motor.forward(0.25)
+		
 		servo_begin(servo=myServo, angle=myAngle)
-
+		print('runtime : '+str(timeit.default_timer() - start))
 
 main()
