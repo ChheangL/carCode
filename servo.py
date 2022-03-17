7eb08d27f5a1c3f2c32e3d311545376a114df190
chheangLy <lychheang008@gmail.com>
2022-03-17 04:35:02 +0000

    fsadf

--- servo.py
+++ servo.py
@@ -3,14 +3,14 @@ from gpiozero import Servo, Motor,Device
 from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
+Device.pin_facotry = PiGPIOFactory()
 #init servo
 gpioPin = 4
 correction = 0.45
-maxpw = (2.0 + correction) / 1000
-minpw = (1.0 - correction) / 1000
-#maxpw = pulse_width(duty_cycle=75)
-#minpw = pulse_width(duty_cycle=25)
-Device.pin_facotry = PiGPIOFactory()
+# maxpw = (2.0 + correction) / 1000
+# minpw = (1.0 - correction) / 1000
+maxpw = pulse_width(duty_cycle=75)
+minpw = pulse_width(duty_cycle=25)
 myServo = Servo(gpioPin, min_pulse_width=minpw, max_pulse_width=maxpw)
 
 #init motor
@@ -18,9 +18,9 @@ print('init motor')
 motor = Motor(forward = 23, backward = 24, enable = 25, pwm=True)
 
 hardwareControl(myServo,100,motor,0)
-sleep(3)
+sleep(5)
 hardwareControl(myServo,80,motor,0)
-sleep(3)
+sleep(5)
 hardwareControl(myServo,100,motor,0)
-sleep(3)
+sleep(5)
 
