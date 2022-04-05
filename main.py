e40c27dca40027d8d982c20bfede9730c90252cc
lychheang <lychheang008@gmail.com>
2022-04-05 11:31:27 +0700

    fjskdfsda

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from sunau import AUDIO_FILE_ENCODING_LINEAR_32
+from sympy import false
 from auto_drive_functions import retrieve_angle
 from ImageFrame import Frame
 # import cv2
 # from bufflessVideoCapture import VideoCapture
 import numpy as np
 from hardwareControl import *
-# import timeit
-
+import timeit
 import cv2, queue, threading, time
 
 # bufferless VideoCapture
@@ -61,33 +62,49 @@ motor = Motor(forward = 23, backward = 24, enable = 25, pwm=True)
 
 
 def main():
-    while True:
-        try:
-#             start = timeit.default_timer()
-            img = cam.read()
+  x = False
+  startTimer = True
+  while True:
+    try:
+      if startTimer :
+        start1 = timeit.default_timer()
+      else :
+        start2 = timeit.default_timer()
+      img = cam.read()
 #             cv2.imshow("frame", img)
 #             cv2.waitKey(1)
 #             print('resolution: ',np.array(img).shape)
-            allPoints,points,mid_points,myAngle = retrieve_angle(s1=50,h1=3, hd=10,layer = 0,img_path=img, frame=frame1)
-            try:
+      _,_,_,myAngle = retrieve_angle(s1=50,h1=3, hd=10,layer = 0,img_path=img, frame=frame1)
 #                 print(myAngle)
-                
-                myAngle = np.mean(myAngle)
-                print(myAngle)
-            except:
-                continue
-            
-            ServoControl(servo, myAngle)
-            if myAngle >-20.0 and myAngle < 20.0:
-              MotorControl(motor, speed=0.25)
-            else:
-              MotorControl(motor, speed=0.15)
+      if not x:
+        Ang1 = np.mean(myAngle)
+        x = True
+        myAngle = 0
+        startTimer = False
+      else:
+        if timeit.default_timer() - start1 >=1.01:
+          myAngle = Ang1
+        elif timeit.default_timer()-start2 >=1.01:
+          myAngle = Ang2
+        else:
+          if startTimer : 
+            Ang1 = np.mean(myAngle)
+          else:
+            Ang2 = np.mean(myAngle)
+        startTimer = not startTimer
+        
+      print(startTimer,' ',myAngle)
+      ServoControl(servo, myAngle)
+      if myAngle >-20.0 and myAngle < 20.0:
+        MotorControl(motor, speed=0.15)
+      else:
+        MotorControl(motor, speed=0.15)
 
 #             print('runtime : '+str(timeit.default_timer() - start))
-        except :
-            servo.detach()
-            motor.stop()
-            break  
+    except :
+        servo.detach()
+        motor.stop()
+        break  
 #     cam.release()
 
 
