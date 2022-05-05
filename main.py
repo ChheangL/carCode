8e3b76036a1b5e6d4fb30f3c88990bc2813b8764
chheangLy <lychheang008@gmail.com>
2022-05-05 04:36:12 +0100

    prepare merge

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main():
     #         cv2.imshow("frame", img)
     #         cv2.waitKey(1)
     #             print('resolution: ',np.array(img).shape)
-            _,points,_,myAngle = retrieve_angle(s1=50,h1=3, hd=20,layer = 0,img_path=img, frame=frame1)
+            _,points,_,myAngle = retrieve_angle(s1=50,h1=5, hd=15,layer = 0,img_path=img, frame=frame1)
             print(points)
     #       if not x:
     #         Ang1 = np.mean(myAngle)
