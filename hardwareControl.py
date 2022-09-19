69419ed7483fa2e9404cd856563b1bb822021052
lychheang <lychheang008@gmail.com>
2022-09-19 15:30:31 +0700

    add hardware

--- hardwareControl.py
+++ hardwareControl.py
@@ -1,31 +1,9 @@
-from gpiozero import Servo, Motor, Device
-from gpiozero.pins.pigpio import PiGPIOFactory
+from smbus import SMBus
 
-def ServoControl(servo, angle):
-    try:
-        angle=angle+90
-        if angle < 45:
-            angle = 45
-        elif angle > 135:
-            angle = 135
-            
-        angleValue = (2/180 * angle - 1)
-        servo.value = angleValue
-#         print("ServoAngle: ", angleValue)
-            
-    except KeyboardInterrupt:
-        servo.detach()
-#         print("Program stopped")
-
-def pulse_width(duty_cycle, period=0.02):
-    result = round(duty_cycle * period / 100, 4)
-#     print('pulse_width: ', result)
-    return result
-
-def degree(pulse_width, max_pulse_width=0.002, degree_of_motor=180):
-    result = round(pulse_width * degree_of_motor / max_pulse_width, 4)
-#     print('pulse_width:', pulse_width, '= degree:', result)
-    return result
-
-def MotorControl(myMotor, speed):
-    myMotor.forward(speed)
+addr = 0x8
+bus = SMBus(1)
+def send(angle1,angle2,duration = ((0x2<<4)+(0x3))):
+    bus.write_byte(addr,angle1)
+    bus.write_byte(addr,duration)
+    bus.write_byte(addr,angle2)
+    bus.write_byte(addr,duration)
