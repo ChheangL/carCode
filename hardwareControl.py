45c471e342e8fb6b7283c7d00147d33c2b43c29f
chheangLy <lychheang008@gmail.com>
2022-09-28 09:22:31 +0100

    Testing2

--- hardwareControl.py
+++ hardwareControl.py
@@ -2,7 +2,7 @@ from smbus import SMBus
 
 addr = 0x8
 bus = SMBus(1)
-def send(angle1,angle2,duration1 = ((0x13)),duration2 = ((0x2<<4)+(0x3))):
+def send(angle1,angle2,duration1 = ((0x23)),duration2 = ((0x2<<4)+(0x3))):
     bus.write_byte(addr,angle1)
     bus.write_byte(addr,duration1)
     bus.write_byte(addr,angle2)
